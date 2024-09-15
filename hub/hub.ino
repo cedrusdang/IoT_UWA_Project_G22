a018522652b1594eeeb50ea1d69b62de8912c36c
CDTT94 <139787921+Cedrus1994@users.noreply.github.com>
2024-09-15 20:08:34 +0800

    ESP~>AWS IOT CORE MQTT

---
+++ hub/hub.ino
@@ -0,0 +1,101 @@
+#include "secrets.h"
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "WiFi.h"
+ 
+#define AWS_IOT_PUBLISH_TOPIC "hub/pub"
+#define AWS_IOT_SUBSCRIBE_TOPIC "hub/sub"
+ 
+float lon ;
+float lat;
+ 
+ 
+WiFiClientSecure net = WiFiClientSecure();
+PubSubClient client(net);
+ 
+void connectAWS()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+ 
+  Serial.println("Connecting to Wi-Fi");
+ 
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  // Configure WiFiClientSecure to use the AWS IoT device credentials
+  net.setCACert(AWS_CERT_CA);
+  net.setCertificate(AWS_CERT_CRT);
+  net.setPrivateKey(AWS_CERT_PRIVATE);
+ 
+  // Connect to the MQTT broker on the AWS endpoint we defined earlier
+  client.setServer(AWS_IOT_ENDPOINT, 8883);
+ 
+  // Create a message handler
+  client.setCallback(messageHandler);
+ 
+  Serial.println("Connecting to AWS IOT");
+ 
+  while (!client.connect(THINGNAME))
+  {
+    Serial.print(".");
+    delay(100);
+  }
+ 
+  if (!client.connected())
+  {
+    Serial.println("AWS IoT Timeout!");
+    return;
+  }
+ 
+  // Subscribe to a topic
+  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+ 
+  Serial.println("AWS IoT Connected!");
+}
+ 
+void publishMessage()
+{
+  StaticJsonDocument<200> doc;
+  doc["Longtiture"] = lon;
+  doc["Latitude"] = lat;
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer); // print to client
+  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+  Serial.print("Payload Size: ");
+  Serial.println(strlen(jsonBuffer));
+  Serial.print(F(" Longtude: "));
+  Serial.println(lon);
+  Serial.print(F(" Latitude: "));
+  Serial.println(lat);
+}
+ 
+void messageHandler(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("incoming: ");
+  Serial.println(topic);
+ 
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, payload);
+  const char* message = doc["message"];
+  Serial.println(message);
+}
+ 
+void setup()
+{
+  Serial.begin(115200);
+  connectAWS();
+}
+ 
+void loop()
+{
+  lon = 200;
+  lat = 100;
+  publishMessage();
+  client.loop();
+  delay(1000);
+}
