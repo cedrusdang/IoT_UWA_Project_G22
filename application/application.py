6a1382391bed8a4d6ee683accbe0a8f7cffb3bf4
Faraz Kabir Khan <45370774+farazkabir@users.noreply.github.com>
2024-10-15 15:53:38 +0800

    Database issue and requirement fix

--- application/application.py
+++ application/application.py
@@ -5,7 +5,7 @@ from routes import main_routes
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/your_database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'SMART_TRACKING'  # Set your secret key
 
